0d8daaa1483eb84329eac73cddc0ba32b9a40a2b
mlum123 <michellelum123@gmail.com>
2022-02-09 16:12:23 -0800

    update index.js and index.css to reflect in class exercise requirements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const data = [
+  {
+    id: 512,
+    name: "Neil Rhodes",
+    email: "rhodes@hmc.edu",
+    phone: "(909) 555-1212",
+  },
+  {
+    id: 787,
+    name: "Barack Obama",
+    email: "ex-prez@whitehouse.gov",
+    phone: "(312) 555-1212",
+  },
+];
+
+function displayPersonInfo(person) {
+  return (
+    <div className="person" key={person.id}>
+      <p>
+        <b>{person.name}</b>
+      </p>
+      <p id="email">{person.email}</p>
+      <p>{person.phone}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>People</h1>
+    {data.map(displayPersonInfo)}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
